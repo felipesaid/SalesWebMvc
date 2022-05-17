6b6d20f88ad59bce95117409649919c58148aef6
felipesaid <felipe.csaid@gmail.com>
2022-05-17 19:09:26 -0300

    Delete seller

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -39,5 +39,6 @@ namespace SalesWebMvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
